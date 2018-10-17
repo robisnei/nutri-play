38e3ae6e5921b05d76da6837005aa51d6b4dcbe1
robisnei <robisneey@hotmail.com>
2018-10-16 23:42:00 -0300

    change ui framework to bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
-
-import './styles/quasar.styl'
-import 'quasar-extras/roboto-font'
-import 'quasar-extras/material-icons'
-import Quasar from 'quasar'
-
-Vue.use(Quasar)
-
-Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
